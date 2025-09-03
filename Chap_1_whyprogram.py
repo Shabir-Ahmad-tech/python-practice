7070f70a0c0e093391e14b2ee21257d473d2d587
Shabir-Ahmad-tech <shabirahmad.tech@gmail.com>
2025-09-04 00:50:09 +0500

    Add Chap_1_whyprogram.py

---
+++ Chap_1_whyprogram.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+fav_Coloue = input("Enter your favourite colour: ")
+fav_Country = input("Enter your favourite country to visit: ")
+
+print("Hello " + name + " your favourite colour is " + fav_Coloue + " and your favourite country to visit is " + fav_Country)
