a7925931e29983354000d7bf55f9eabec14960a0
Shabir-Ahmad-tech <shabirahmad.tech@gmail.com>
2025-09-04 01:56:56 +0500

    Added new program

---
+++ Chap_1_whyprogram2.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+fav_Coloue = input("Enter your favourite colour: ")
+fav_Country = input("Enter your favourite country to visit: ")
+
+# print every information on separate line
+
+print("Hello " + name + 
+      "\nYour favourite colour is " + fav_Coloue + 
+      "\nYour favourite country to visit is " + fav_Country)
