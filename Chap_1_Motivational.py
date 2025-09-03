f3ce9dfabc25e157314b7d330e565cc9ae48da1c
Shabir-Ahmad-tech <shabirahmad.tech@gmail.com>
2025-09-04 02:22:05 +0500

    Added a new program

---
+++ Chap_1_Motivational.py
@@ -0,0 +1,25 @@
+# Print a motivational quote
+num = int(input("Enter a number between 1 and 10: "))
+
+if num == 1:
+    print("Keep going!")
+elif num == 2:
+    print("You can do it!")
+elif num == 3:
+    print("You are a great person, keep going!")
+elif num == 4:
+    print("Dn't Give Up!")
+elif num == 5:
+    print("Nothing is impossible!")
+elif num == 6:
+    print("Do your best!")
+elif num == 7:
+    print("Optimizm is the key to success!")
+elif num == 8:
+    print("Believe in yourself!")
+elif num == 9:
+    print("Work hard, play hard!")
+elif num == 10:
+    print("You got this!")
+else:
+    print("Invalid input. Please enter a number between 1 and 10.")
