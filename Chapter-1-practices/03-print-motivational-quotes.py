05f6a6c691b90a2917da893fd795be48a632448e
Shabir-Ahmad-tech <shabirahmad.tech@gmail.com>
2025-09-04 02:27:30 +0500

    added new folders and update the motivational

---
+++ Chapter-1-practices/03-print-motivational-quotes.py
@@ -0,0 +1,12 @@
+# Print a motivational quote
+
+print("Keep going!")
+print("You can do it!")
+print("You are a great person, keep going!")
+print("Dn't Give Up!")
+print("Nothing is impossible!")
+print("Do your best!")
+print("Optimizm is the key to success!")
+print("Believe in yourself!")
+print("Work hard, play hard!")
+print("You got this!")
